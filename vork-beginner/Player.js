b367755d2c6e4e1e543b5ceb43ba56b2bb8644b5
Erik Scholten <erik.scholten@trimm.nl>
2018-05-16 07:44:14 +0200

    level 6

--- vork-beginner/Player.js
+++ vork-beginner/Player.js
@@ -1,25 +1,212 @@
+
+const DIRECTIONS = ['forward', 'right', 'backward', 'left'];
+
 class Player {
 
   constructor() {
-    this.health = 20;
+    this.health = 22;
+    this.isRunningAway = false;
+    this.direction = 'backward';
   }
 
   playTurn(warrior) {
-    if (!this.wasAttackedLastTurn(warrior) && this.isInjured(warrior) && !warrior.feel().isEnemy()) {
-      warrior.rest();
-    } else if (warrior.feel().isEnemy()) {
-      warrior.attack();
-    } else {
-      warrior.walk();
+
+    //if(warrior.feel(this.direction).isWall()) {
+      //this.turnLeft();
+      //this.turnOpposite();
+      //
+      // this.direction = 'forward';
+    //}
+
+    // Check if we need to evade ranged attacks until healed
+    if (this.shouldEvadeAttack(warrior)) {
+
     }
 
+    // should warrior take a rest
+    else if (this.shouldTakeARest(warrior)) {
+
+    }
+
+    // rescue captives
+    else if (this.rescueCaptive(warrior)) {
+
+    }
+
+    // attack nearby enemies
+    else if (this.attackEnemy(warrior)) {
+
+    }
+
+    // find your exit
+    else {
+      this.moveToStairs(warrior)
+    }
+
+    this.storeState(warrior);
+
+    // test if we should change our direction
+    this.shouldChangeDirection(warrior);
+
+  }
+
+  storeState(warrior) {
     this.health = warrior.health();
   }
 
+  /**
+   * Change direction
+   *
+   * @param warrior
+   */
+  shouldChangeDirection(warrior) {
+      if (warrior.feel(this.direction).isWall()) {
+       this.turnLeft();
+        //this.shouldChangeDirection(warrior);
+      }
+  }
+
+  /**
+   * Allways turn left
+   */
+  turnLeft() {
+    switch(this.direction) {
+      case 'backward':
+        return this.direction = 'left';
+        break;
+      case 'left':
+        return this.direction = 'forward';
+        break;
+      case 'forward':
+        return this.direction = 'right';
+        break;
+      case 'right':
+        return this.direction = 'backward';
+        break;
+    }
+    return true;
+  }
+
+  /**
+   * Run forest Run
+   */
+  turnOpposite() {
+    switch(this.direction) {
+      case 'backward':
+        this.direction = 'forward';
+        break;
+      case 'left':
+        this.direction = 'right';
+        break;
+      case 'forward':
+        this.direction = 'backward';
+        break;
+      case 'right':
+        this.direction = 'left';
+    }
+  }
+
+  /**
+   * Find your exit
+   *
+   * @param warrior
+   */
+  moveToStairs(warrior) {
+    warrior.walk(this.direction);
+
+    return true;
+  }
+
+  /**
+   * Attack anamy if present
+   * @param warrior
+   */
+  attackEnemy(warrior) {
+    if (warrior.feel(this.direction).isEnemy()) {
+      warrior.attack(this.direction);
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Rescue a captive
+   * @param warrior
+   */
+  rescueCaptive(warrior) {
+    if (warrior.feel(this.direction).isCaptive()) {
+      warrior.rescue(this.direction);
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * I was attacked but no enemy was near
+   * start moving in the opposite direction
+   * @param warrior
+   */
+  shouldEvadeAttack(warrior) {
+    if (!this.isRunningAway && this.wasAttackedLastTurn(warrior) && warrior.health() <= 10 && !this.adjacentToEnemy(warrior)) {
+
+      this.turnOpposite();
+
+      this.isRunningAway = true;
+
+      warrior.walk(this.direction);
+
+      return true;
+    }
+
+    // We can stop running and turn the opposite way again
+    else if (this.isRunningAway && !this.wasAttackedLastTurn()) {
+
+      this.turnOpposite();
+
+      this.isRunningAway = false;
+
+      return true;
+    }
+
+    return false;
+  }
+
+  /**
+   *
+   * @param warrior
+   * @returns {Array}
+   */
+  adjacentToEnemy(warrior) {
+    return DIRECTIONS.some((direction) => warrior.feel(direction).isEnemy());
+  }
+
+  /**
+   * Warrior is not under attack, has been injured and is not in front of an enemy
+   * @param warrior
+   * @returns {boolean|*}
+   */
+  shouldTakeARest(warrior) {
+    if (!this.wasAttackedLastTurn(warrior) && this.isInjured(warrior) && !warrior.feel(this.direction).isEnemy()) {
+      warrior.rest();
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Check if warrior has been injured
+   * @param warrior
+   * @returns {boolean}
+   */
   isInjured(warrior) {
     return warrior.health() < 20;
   }
 
+  /**
+   * Check if a attack has happened since last turn.
+   * @param warrior
+   * @returns {boolean}
+   */
   wasAttackedLastTurn(warrior) {
     return warrior.health() < this.health;
   }
--- vork-beginner/README.md
+++ vork-beginner/README.md
@@ -1,21 +1,22 @@
-## vork - beginner - level 4
+## vork - beginner - level 6
 
-### _You can hear bow strings being stretched._
+### _The wall behind you feels a bit further away in this room. And you hear more cries for help._
 
-> **TIP:** No new abilities this time, but you must be careful not to rest while taking damage. Save a `health` variable and compare it on each turn to see if you're taking damage.
+> **TIP:** You can walk backward by passing `'backward'` as an argument to `walk()`. Same goes for `feel()`, `rescue()` and `attack()`. Archers have a limited attack distance.
 
+> **CLUE:** Walk backward if you're taking damage from afar and don't have enough health to attack. You may also want to consider walking backward until you hit a wall. Use `warrior.feel().isWall()` to see if there's a wall.
 
 ### Floor Map
 
 ```
-╔═══════╗
-║@ Sa S>║
-╚═══════╝
+╔════════╗
+║C @ S aa║
+╚════════╝
 
+C = Captive (1 HP)
 @ = vork (20 HP)
 S = Thick Sludge (24 HP)
 a = Archer (7 HP)
-> = stairs
 ```
 
 ### Abilities
@@ -25,6 +26,7 @@ a = Archer (7 HP)
 * `warrior.walk()`: Move one space in the given direction (forward by default).
 * `warrior.attack()`: Attack a unit in the given direction (forward by default) dealing 5 HP of damage.
 * `warrior.rest()`: Gain 10% of max health back, but do nothing more.
+* `warrior.rescue()`: Rescue a captive from his chains (earning a reward) in the given direction (forward by default).
 
 #### Senses
 
